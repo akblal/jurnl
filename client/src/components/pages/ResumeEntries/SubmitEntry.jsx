13270f1a1b288a6ca5cf6243b439829358445ee4
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-08-09 00:11:30 -0700

    added text input field for synonyms entry

--- client/src/components/pages/ResumeEntries/SubmitEntry.jsx
+++ client/src/components/pages/ResumeEntries/SubmitEntry.jsx
@@ -3,28 +3,49 @@ import React, { useState } from 'react';
 function SubmitEntry () {
 
   const [bulletPoint, setBulletPoint] = useState('');
-  const [validInput, setValidInput] = useState(false);
+  const [validBullet, setValidBullet] = useState(false);
+  const [findSynonym, setFindSynonym] = useState('');
+  const [validSynonym, setValidSynonym] = useState(false);
 
   const handleBulletPoint = (event) => {
     setBulletPoint(event.target.value);
     if (event.target.value.length > 0) {
-      setValidInput(true);
+      setValidBullet(true);
     } else {
-      setValidInput(false);
+      setValidBullet(false);
     }
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmitPoint = (event) => {
     event.preventDefault();
-    console.log (bulletPoint);
+    console.log (bulletPoint, 'point to be added in resume');
+  }
+
+  const handleFindSynonym = (event) => {
+    setFindSynonym(event.target.value);
+    if (event.target.value.length > 0) {
+      setValidSynonym(true);
+    } else {
+      setValidSynonym(false);
+    }
+  }
+
+  const handleSubmitSynonym = (event) => {
+    event.preventDefault();
+    console.log (findSynonym, 'synonym to look for')
   }
 
   return (
     <div>
-      <h2 className = 'submit-entry-title'>Impressive Achievement</h2>
-      <div className = 'submit-entry-container'>
+      <h2 className= 'submit-entry-title'>Spectacular Achievement</h2>
+      <div className= 'submit-entry-container'>
         <input value= {bulletPoint} onChange= {handleBulletPoint} placeholder= 'Awesome Idea Here' size="50" className= 'resume-text-field'></input>
-        <button onClick= {handleSubmit} disabled = {!validInput}>I Am Very Impressed!</button>
+        <button onClick= {handleSubmitPoint} disabled = {!validBullet}>I Am Very Impressed!</button>
+      </div>
+      <h2 className= "submit-synonym-title">Word Lookup: </h2>
+      <div className = 'thesaurus-entry-container'>
+        <input value= {findSynonym} onChange= {handleFindSynonym} placeholder= 'Find Synonym' size="30" className= 'synonym-text-field'></input>
+        <button onClick= {handleSubmitSynonym} disabled = {!validSynonym}>Find Synonym!</button>
       </div>
     </div>
   )
